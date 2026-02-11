dcacf8d03fb2a415c99ce2bd439815e53ab46691
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2026-02-11 23:30:39 +0330

    init

---
+++ go.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	. "fmt"
+
+	"github.com/mostafasolati/goodbye"
+)
+
+func main() {
+	Println("hello world")
+	goodbye.SayGoodbye("")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mosayyebnezhad/quera
+
+go 1.25.0
+
+require github.com/mostafasolati/goodbye v1.0.0 // indirect
